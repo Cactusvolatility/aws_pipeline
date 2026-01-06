9e7d17f15771d161b3bd24b3e4aa2ac81d2d3c3e
Cactusvolatility <pyang70311@gmail.com>
2026-01-05 22:25:57 -0800

    update 5min python lambda

--- lambda/py-five-interval/handler.py
+++ lambda/py-five-interval/handler.py
@@ -1,2 +1,37 @@
-def handler(event, context):
-    return {"Hello Python": True}
+import polars as pl
+import boto3
+from datetime import datetime, timezone
+
+s3_client = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+
+BUCKET = 'semidata-lake-123456'
+State_table = 'ticker-statistics'
+
+def trigger_five_handler(event, context):
+    print("trigger lambda for processing data")
+
+    today = datetime.now(timezone.utc).strftime("%Y-%m-%d")
+
+    prefix = f'prices/tiingo_book/date=2026-01-05/'
+
+    resp = s3_client.list_objects_v2(
+        Bucket = BUCKET,
+        Prefix = prefix,
+    )
+
+    if "Contents" not in resp:
+        print("No files found")
+        return {"files": []}
+
+    keys = [obj["Key"] for obj in resp["Contents"]]
+
+    print("Found files:")
+    for k in keys:
+        print(k)
+
+    return {
+        "date": today,
+        "file_count": len(keys),
+        "files": keys,
+    }
